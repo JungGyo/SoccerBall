c294f9aafdf3e8868b125c8b60dcb07a48b8d3f1
elabor.han <elabor.han@gmail.com>
2020-12-02 13:05:45 +0900

    Add script for drawing Ball's foot print

---
+++ Assets/FootPrint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootPrint : MonoBehaviour
+{
+    public Transform footPrintGroup;
+    public GameObject footPrint;
+    List<GameObject> footPrintPool;
+
+    void Awake(){
+        if (footPrintPool == null){
+            footPrintPool = new List<GameObject>();
+            for(int i = 0; i < 20; i++)
+                footPrintPool.Add(Instantiate(footPrint, footPrintGroup));
+        }
+    }
+
+    public void Draw(Vector3 pos){
+        var obj = Instantiate(footPrint, footPrintGroup);
+        footPrintPool.Add(obj);
+        footPrint.transform.position = pos;
+    }
+}
