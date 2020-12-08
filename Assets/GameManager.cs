92f38bbbbf9ac250b3249c111f46306e5ea13a4e
elabor.han <elabor.han@gmail.com>
2020-12-08 20:06:31 +0900

    병진 운동 마찰, 수직항력(반력), 충돌 테스트 완료.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,14 +8,20 @@ public class GameManager : MonoBehaviour
     public Transform ballParent;
     Ball ball;
 
+    public RigidbodyObject rigidbodyObject;
+    public Rigidbody untiyPhysicsObject;
+
+    public Vector3 pos0;
+    public Vector3 posUnity;
     public Vector3 vel0 = new Vector3(0, 10f, -20f);  
     public Vector3 rotVel0 = new Vector3();
 
     public bool onFootPrint = false;
     public bool onDrag = true;
-    public bool onSpinDrag = true;
-    public bool onSpinForce = true;
-    public float spinFactor = 1f;
+    public bool onAngularDrag = true;
+    public bool onAngularForce = true;
+    public float Drag = 1f;
+    public float AngularDrag = 1f;
 
     void Awake(){
         Time.fixedDeltaTime = 0.005f;
@@ -23,24 +29,33 @@ public class GameManager : MonoBehaviour
 
     void Start(){
         ballPrefab.gameObject.SetActive(false);
+        pos0 = rigidbodyObject.transform.position;
+        posUnity = untiyPhysicsObject.transform.position;
         OnSpawnBall();
     }
 
     public void OnSpawnBall(){
-        if (ball != null){
-            Destroy(ball.gameObject);
-        }
-        ball = Instantiate(ballPrefab, ballParent);
-        ball.gameObject.SetActive(true);
-        ball.Spawn(ball.transform.position);
+        // if (ball != null){
+        //     Destroy(ball.gameObject);
+        // }
+        // ball = Instantiate(ballPrefab, ballParent);
+        // ball.gameObject.SetActive(true);
+        // ball.Spawn(ball.transform.position);
+
+        rigidbodyObject.transform.position = pos0;
+        untiyPhysicsObject.transform.position = posUnity;
     }
 
     public void OnShoot(){
-        ball.onDrag = onDrag;
-        ball.onSpinDrag = onSpinDrag;
-        ball.onSpinForce = onSpinForce;
-        ball.spinFactor = spinFactor;
-        ball.onFootPrint = onFootPrint;
-        ball.Shoot(vel0, rotVel0);
+        // ball.onDrag = onDrag;
+        // ball.onAngularDrag = onAngularDrag;
+        // ball.onAngularForce = onAngularForce;
+        // ball.drag = Drag;
+        // ball.angularDrag = AngularDrag;
+        // ball.onFootPrint = onFootPrint;
+        // ball.Shoot(vel0, rotVel0);
+
+        rigidbodyObject.velocity = vel0;
+        untiyPhysicsObject.velocity = vel0;
     }
 }
--- Assets/RigidbodyObject.cs
+++ Assets/RigidbodyObject.cs
@@ -11,42 +11,69 @@ public class RigidbodyObject : MonoBehaviour
     float mass {get{return rigidbody.mass;}}
     bool isMoving {get{return velocity.magnitude != 0;}}
 
+    public float Boundness {
+        get {return bounceness;}
+        set {bounceness = Mathf.Clamp(value, 0, 1);}
+    }
+    public float contactReward = 1f;
     public float bounceness = 0.5f; //콘크리트에서 축구공의 반발계수 0.77~0.9 (콘크리트)
     public float dynamicFriction = 0.6f;
     public float staticFriction = 0.4f;
     [HideInInspector] public Vector3 velocity = new Vector3();
     Vector3 pos {
         get{ return this.transform.position; }
-        set{ this.transform.position = value; }
+        set{
+// #if UNITY_EDITOR
+//             if (UnityEditor.Tools.current == UnityEditor.Tool.Move &&
+//                 UnityEditor.Tools.handlePosition == this.transform.position)
+//                 return;
+// #endif
+
+            this.transform.position = value;
+            }
     }
     Vector3 acc = new Vector3();
 
     Rigidbody rigidbody;
     void Awake(){
         rigidbody = GetComponent<Rigidbody>();
+        rigidbody.isKinematic = true;
     }
 
-    // Update is called once per frame
     void FixedUpdate(){
         var delta = Time.fixedDeltaTime;
 
         var friction = Vector3.zero;
         var reaction = Vector3.zero;
+        var collisionReact = Vector3.zero;
         if (onContacted){
-            foreach(var point in contactDic.Values){
+            foreach(var key in contactDic.Keys){
+                var point = contactDic[key]; 
                 var nVector = point.normal.normalized;
-                velocity = Vector3.Scale(Vector3.Reflect(velocity, nVector), new Vector3(1, bounceness, 1));
-
-                friction += CalcFriction(velocity, acc, nVector);
-                reaction += CalcReactionForce(acc, nVector);
+                if (reflectionDone.Contains(key) == false){
+                    velocity = Vector3.Scale(Vector3.Reflect(velocity, nVector), new Vector3(1, bounceness, 1));
+                    reflectionDone.Add(key);
+                }
+                friction += CalcFriction(velocity, gravity, nVector);
+                reaction += CalcReactionForce(gravity, nVector);
             }
         }
 
+        acc = gravity + (friction + reaction)/mass;
         pos = CalcPos(pos, velocity, acc, delta);
         velocity = CalcVel(velocity, acc, delta);
-        acc = gravity + (friction + reaction)/mass;
 
-        this.transform.position = pos;
+        // Debug.Log($"Friction: {friction}/ Reaction:{reaction} /vel: {velocity}/acc: {acc}");
+        Debug.Log($"pos: {pos.y} /vel: {velocity.y} /acc: {acc.y} /fr: {friction}/ ra:{reaction}");
+    }
+
+    float time;
+    void Update(){
+        if((int)(time * 10 % 5) == 0){
+            //Debug.Log($"vel: {velocity}/acc: {acc}");
+        }
+
+        time += Time.deltaTime;
     }
 
     Vector3 CalcPos(Vector3 p0, Vector3 v0, Vector3 a0, float t){
@@ -63,41 +90,56 @@ public class RigidbodyObject : MonoBehaviour
 
     //List<ContactPoint> contactPoints = new List<ContactPoint>();
     Dictionary<Collider, ContactPoint> contactDic = new Dictionary<Collider, ContactPoint>();
+    HashSet<Collider> reflectionDone = new HashSet<Collider>();
     public bool onContacted {get{return contactDic.Count > 0;}}
 
     void OnCollisionEnter(Collision col){
         if (isMoving == false)
             return;
-        //contactPoints.Add(col.GetContact(0));
+
         contactDic[col.collider] = col.GetContact(0);
-        // if (onFootPrint) footPrint.Draw(pos);
+
+        //RigidbodyObject가 아닌 isKinematic rigidbody인 경우 위치 보정.
+        if (col.gameObject.GetComponent<RigidbodyObject>() == null)
+            pos -= col.GetContact(0).normal.normalized * col.GetContact(0).separation;
+
         Debug.Log($"name: {col.gameObject.name} - OnCollisionEnter({col.contacts.Length})/ contact nv: {col.GetContact(0).normal.normalized}");
     }
     void OnCollisionStay(Collision col){
-        //lastContactPoint.normal.normalized;
-        // reflectionVectors.Add(contactNormalVector);
-        // Debug.Log($"time: {time} - OnCollisionStay({col.contacts.Length})");
+        if (reflectionDone.Contains(col.collider)){
+            contactDic[col.collider] = col.GetContact(0);
+        }
     }
 
     void OnCollisionExit(Collision col){
-        if (contactDic.ContainsKey(col.collider))
-            contactDic.Remove(col.collider);
+        var collider = col.collider;
+        if (contactDic.ContainsKey(collider))
+            contactDic.Remove(collider);
+        if (reflectionDone.Contains(collider))
+            reflectionDone.Remove(collider);
         Debug.Log($"name: {col.gameObject.name} - OnCollisionExit({col.contacts.Length})");
     }
 
     Vector3 CalcFriction(Vector3 vel, Vector3 acc, Vector3 nVector){
-        var n = -1 * vel.normalized;
-        var normalForceScale = Vector3.Dot(acc, nVector.normalized);
-        var force = dynamicFriction * normalForceScale * mass * n;
-        // Debug.Log($"CalcFriction - F: {force}/accMag: {normalForceScale}/normal vector:{n}");
+        var n = nVector.normalized;
+        var normalForceScale = Vector3.Dot(acc, -n);
+        var u = vel - Vector3.Project(vel, n);
+        var force = normalForceScale > 0? dynamicFriction * normalForceScale * mass * -u : Vector3.zero;
+        // Debug.Log($"CalcFriction - F: {force}/nVector:{n}/ uVector:{-u}");
         return force;
     }
 
-    //Wall은 질량이 무하다고 가정. 벽의 수직 방향 가속도는 0이 되어야함.(수직항력 or 반력)
+    //TODO: Wall이 아닌 RigidObject에 대한 반력 계산으로 수정되어야함.
+    //Wall은 질량이 무한하다고 가정시 수직 방향 가속도는 0이 되어야함.(수직항력 or 반력)
     Vector3 CalcReactionForce(Vector3 acc, Vector3 nVector){
         var n = nVector.normalized;
-        var tangentForce = (acc - Vector3.Dot(acc, n) * n) * mass;
-        // Debug.Log($"CalcReactionForceAcc - acc: {acc}/accNew: {newAcc}/normal vector:{n}");
+        var tangentForce = (acc - Vector3.Project(acc, n)) * mass;
+        // Debug.Log($"CalcReactionForce - F: {tangentForce}/nVector:{n}");
         return tangentForce - (acc * mass);
+    }
+
+    Vector3 CalcContactRewardVelocity(Vector3 nVector, float dist, float t){
+        var n = nVector.normalized;
+        return  (n * dist) / t * contactReward;
     }
 }
